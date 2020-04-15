f27c04cf2458010590fbeffca79d33f43dcf8bd2
chen long <cl7416@126.com>
2020-04-15 20:29:13 +0800

    移除邮件发送的 from 选项

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -29,7 +29,7 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-        if (Auth::attempt($credentials, $request->has('remenber'))) {
+        if (Auth::attempt($credentials, $request->has('remember'))) {
             if (Auth::user()->activated) {
                 session()->flash('success', '欢迎回来！');
                 $fallback = route('users.show', Auth::user());
